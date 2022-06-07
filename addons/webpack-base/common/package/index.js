48fc034a7f33eca2ec63c92b2e78a89b883fa703
ulises-jeremias <ulisescf.24@gmail.com>
2022-06-06 23:08:15 -0300

    Updated scripts

--- addons/webpack-base/common/package/index.js
+++ addons/webpack-base/common/package/index.js
@@ -16,11 +16,13 @@ module.exports = function resolvePackage(setup, { appName, command, srcDir }) {
     },
     scripts: {
       "build:dev": `webpack --env env=development`,
+      "build:dev:analyze": `${command} build:dev --env addon=bundleanalyze`,
+      "build:dev:visualize": `${command} build:dev --env addon=bundlevisualizer`,
       "build:dev:watch": `${command} build:dev --watch --hot`,
-      "build:dev:analyze": `${command} build:dev --env addon=bundleanalyze --env addon=bundlevisualizer`,
       build: `webpack --env env=production`,
+      "build:analyze": `${command} build --env addon=bundleanalyze`,
+      "build:visualize": `${command} build --env addon=bundlevisualizer`,
       "build:watch": `${command} build --watch`,
-      "build:analyze": `${command} build --env addon=bundleanalyze --env addon=bundlevisualizer`,
       lint: `prettier --ignore-path .eslintignore --check \"**/*.{js,jsx,json,css,sass,scss,less,html,md}\" && eslint ${srcDir}`,
       "lint:fix": `prettier --ignore-path .eslintignore --write \"**/*.{js,jsx,json,css,sass,scss,less,html,md}\" && eslint ${srcDir} --fix`,
       "serve:dev":
--- addons/webpack-base/common/template/webpack.config.js
+++ addons/webpack-base/common/template/webpack.config.js
@@ -2,15 +2,17 @@ const { merge } = require('webpack-merge');
 
 const commonConfig = require('./config/webpack.common.js');
 
-const getAddons = addonsArgs => {
-  const addons = Array.isArray(addonsArgs)
-    ? addonsArgs
-    : [addonsArgs];
-
-  return addons
+/**
+ * getAddons is a function that returns valid addon modules
+ *
+ * @param {string} addons - List of addons separated by comma
+ * @returns required addons modules
+ */
+const getAddons = (addons) =>
+  addons
+    .split(',')
     .filter(Boolean)
-    .map(name => require(`./config/addons/webpack.${name}.js`));
-};
+    .map((name) => require(`./config/addons/webpack.${name}.js`));
 
 module.exports = ({ env, addon }) => {
   const envConfig = require(`./config/webpack.${env || 'production'}.js`);
