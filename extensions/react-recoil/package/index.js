067054dcd9dbd1d75b645825677d8d6629ee979f
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2023-03-09 01:37:12 -0300

    Update index.js

--- extensions/react-recoil/package/index.js
+++ extensions/react-recoil/package/index.js
@@ -1,10 +1,10 @@
 module.exports = function resolvePackage(setup) {
   const dependencies = {
     recoil: "^0.7.7",
-    "recoil-devtools": "^0.4.0",
-    "recoil-devtools-dock": "^0.4.0",
-    "recoil-devtools-log-monitor": "^0.4.0",
-    "recoil-devtools-logger": "^0.4.0",
+    "recoil-devtools": "^0.5.0",
+    "recoil-devtools-dock": "^0.5.0",
+    "recoil-devtools-log-monitor": "^0.5.0",
+    "recoil-devtools-logger": "^0.5.0",
   };
 
   return {
