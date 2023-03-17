d56c3543df6cddd237e6b776c7a75846e9dbd258
ulises-jeremias <ulisescf.24@gmail.com>
2023-03-17 17:32:50 -0300

    Added new template

---
+++ templates/webextension-react-vite-starter/package/dependencies.js
@@ -0,0 +1,7 @@
+module.exports = {
+  react: "^18.1.0",
+  "react-dom": "^18.1.0",
+  "react-router-dom": "^6.3.0",
+  "web-vitals": "^2.1.4",
+  "webextension-polyfill": "^0.10.0",
+};
---
+++ templates/webextension-react-vite-starter/package/index.js
@@ -0,0 +1,42 @@
+const dependencies = require("./dependencies");
+const devDependencies = require("./devDependencies");
+
+module.exports = function resolvePackage(setup, { appName, command, srcDir }) {
+  const packageJson = {
+    name: appName,
+    version: "0.1.0",
+    private: true,
+    engines: {
+      node: ">=18.15",
+    },
+    browserslist: {
+      production: [">0.2%", "not dead", "not op_mini all"],
+      development: [
+        "last 1 chrome version",
+        "last 1 firefox version",
+        "last 1 safari version",
+      ],
+    },
+    scripts: {
+      // prepare: "is-ci || husky install",
+      build: "tsc && vite build",
+      "build:watch": "NODE_ENV=development vite build --watch",
+      wss: "ts-node .webext-config/reload/initReloadServer.ts",
+      dev: "(run-p wss build:watch)",
+      preview: "vite preview",
+      format:
+        'prettier --write "**/*.{js,jsx,ts,tsx,json,css,sass,scss,less,html,md,yml,yaml}"',
+      lint: "eslint .",
+      "lint:fix": "eslint . --fix",
+      start: `${command} dev`,
+      "lint-staged": "lint-staged",
+      typecheck: "tsc --noEmit",
+    },
+    "lint-staged": {
+      "*.{js,jsx,ts,tsx}": ["prettier --write", "npm run lint:fix"],
+      "*.{json,css,sass,scss,less,html,md,yml,yaml}": ["prettier --write"],
+    },
+  };
+
+  return { ...packageJson, dependencies, devDependencies };
+};
