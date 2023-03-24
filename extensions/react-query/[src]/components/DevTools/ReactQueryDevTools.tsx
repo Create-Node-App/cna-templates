609fd81ce6f035431004ebae8d93dd0894e8b31e
Tito Molina <roberto.molina@nan-labs.com>
2023-03-24 17:02:31 -0300

    adding react query devtools to template

---
+++ extensions/react-query/[src]/components/DevTools/ReactQueryDevTools.tsx
@@ -0,0 +1,7 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export const ReactQueryDevTools = () => {
+  return (
+    <ReactQueryDevtools initialIsOpen={false} />
+  );
+};
