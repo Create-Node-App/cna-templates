e437e3099b734c6e8ffbbcac13a6308714d8eae6
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2023-04-26 12:55:19 -0300

    Update global.d.ts

--- templates/webextension-react-vite-starter/[src]/global.d.ts
+++ templates/webextension-react-vite-starter/[src]/global.d.ts
@@ -16,7 +16,7 @@ declare module "virtual:reload-on-update-in-view" {
 
 declare module "*.svg" {
   import React = require("react");
-  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
   const src: string;
   export default src;
 }
