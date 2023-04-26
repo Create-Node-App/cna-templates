6ff620cf94ff6559a857f2e1b3733c9ddec73964
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2023-04-26 12:53:49 -0300

    Update global.d.ts

--- templates/react-vite-starter/[src]/global.d.ts
+++ templates/react-vite-starter/[src]/global.d.ts
@@ -1,6 +1,6 @@
 declare module "*.svg" {
   import React = require("react");
-  export const ReactComponent: React.SFC<React.SVGProps<SVGElement>>;
+  export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
   const src: string;
   export default src;
 }
