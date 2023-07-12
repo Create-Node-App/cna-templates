884554c340371b0f61a4e08fa169bf7b0a7c4d23
AgusPk <agustinperkins@hotmail.com>
2023-07-12 19:53:37 -0300

    added migrations to drizzle sqlite extension

--- extensions/nestjs-drizzle-sqlite/src/app.module.ts
+++ extensions/nestjs-drizzle-sqlite/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DrizzleProvider } from './db/drizzle.provider';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, DrizzleProvider],
 })
--- extensions/nestjs-drizzle-sqlite/src/db/drizzle.provider.ts
+++ extensions/nestjs-drizzle-sqlite/src/db/drizzle.provider.ts
@@ -1,14 +1,35 @@
 import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import Database from 'better-sqlite3';
+import { ConfigService } from '@nestjs/config';
+import path from 'path';
 
 @Injectable()
 export class DrizzleProvider implements OnModuleInit {
   db: BetterSQLite3Database;
 
+  constructor(private readonly configService: ConfigService) {}
+
   async onModuleInit() {
-    const sqlite = new Database('sqlite.db');
-    this.db = drizzle(sqlite);
+    this.db = drizzle(this.createDb());
+    //IMPORTANT: After defining the database schema, run the command "npm run migrate"
+    //then uncomment the following line!
+    //this.migrateDb();
+  }
+
+  private createDb() {
+    const sqlite_db_name = this.configService.get<string>('SQLITE_DATABASE_NAME') + '.db';
+    return new Database(sqlite_db_name);
+  }
+
+  private getMigrationsFolder() {
+    const stage = this.configService.get<string>('STAGE');
+    return stage === 'local' ? './src/db/migrations' : path.join(__dirname, 'migrations');
+  }
+
+  private migrateDb() {
+    const migrationsFolder = this.getMigrationsFolder();
+    migrate(this.db, { migrationsFolder: migrationsFolder });
   }
 }
