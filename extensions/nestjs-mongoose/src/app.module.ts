822fc27419c31351004ac886f94b0e49dc75ff41
AgusPk <agustinperkins@hotmail.com>
2023-07-30 14:54:39 -0300

    added nestjs-mongoose extension template

---
+++ extensions/nestjs-mongoose/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { mongooseConfig } from './helpers/mongoose.config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: mongooseConfig,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
