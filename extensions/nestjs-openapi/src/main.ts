379410c8551c1e45fb8eae513d3028a9f6148855
AgusPk <agustinperkins@hotmail.com>
2023-10-04 14:34:02 -0300

    added nestjs-openapi extension

---
+++ extensions/nestjs-openapi/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('NESTJS API')
+    .setDescription('REST API for NESTJS')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
