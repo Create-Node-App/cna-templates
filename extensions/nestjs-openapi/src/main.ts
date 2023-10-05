f77114c5d7ce3292c95e2be689a77e89b5bd6514
AgusPk <agustinperkins@hotmail.com>
2023-10-05 17:16:06 -0300

    added port const

--- extensions/nestjs-openapi/src/main.ts
+++ extensions/nestjs-openapi/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
--- templates/nestjs-starter/src/main.ts
+++ templates/nestjs-starter/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
