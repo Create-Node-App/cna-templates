0fe21e7f9fbddd24c292779b17b22246ed54896d
ulises-jeremias <ulisescf.24@gmail.com>
2025-02-16 05:08:09 -0300

    fix: update TypeScript declaration for image and JSON modules to improve compatibility

--- templates/react-vite-starter/[src]/global.d.ts
+++ templates/react-vite-starter/[src]/global.d.ts
@@ -1,21 +1,22 @@
-declare module "*.svg" {
-  import React = require("react");
+declare module '*.svg' {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  import React = require('react');
   export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
   const src: string;
   export default src;
 }
 
-declare module "*.jpg" {
+declare module '*.jpg' {
   const content: string;
   export default content;
 }
 
-declare module "*.png" {
+declare module '*.png' {
   const content: string;
   export default content;
 }
 
-declare module "*.json" {
+declare module '*.json' {
   const content: string;
   export default content;
 }
