20e0c55d13c27efd6b164a364d466b170b50b85b
ulises-jeremias <ulisescf.24@gmail.com>
2026-06-24 15:03:50 -0300

    fix: preserve standalone extension integrations

--- extensions/react-query/[src]/components/DevTools/ReactQueryDevTools.tsx
+++ extensions/react-query/[src]/components/DevTools/ReactQueryDevTools.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const ReactQueryDevTools = () => {
-  return (
-    <ReactQueryDevtools initialIsOpen={false} />
-  );
+  return <ReactQueryDevtools initialIsOpen={false} />;
 };
---
+++ extensions/react-redux-thunk/[src]/reducers/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+  // Add your slice reducers here
+});
+
+export default rootReducer;
