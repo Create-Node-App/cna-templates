632812f417f55ed5967f8dcc1762a2201b60ead9
ulises-jeremias <ulisescf.24@gmail.com>
2026-06-24 13:54:59 -0300

    fix: stabilize full template combinations

--- extensions/react-redux-thunk/[src]/reducers/index.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
-const rootReducer = combineReducers({
-  // Add your slice reducers here
-});
-
-export default rootReducer;
--- extensions/react-redux-thunk/[src]/types/store.ts
+++
@@ -1,4 +0,0 @@
-import type { store } from '<%= projectImportPath %>store';
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
