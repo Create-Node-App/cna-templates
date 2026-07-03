54fb734cd0680b3191113cbc5f1d4714b6217c44
Ulises Jeremias <ulisescf.24@gmail.com>
2026-07-03 19:07:37 -0300

    fix(ci): remove useless try/catch in nestjs-drizzle-postgres asm helper (#148)

--- extensions/nestjs-drizzle-postgres/src/helpers/asm.ts
+++ extensions/nestjs-drizzle-postgres/src/helpers/asm.ts
@@ -14,17 +14,11 @@ export const getSecretsManagerClient = () => {
 
 export const getSecretValue = async (secretName: string) => {
   const client = getSecretsManagerClient();
-  try {
-    const command = new GetSecretValueCommand({
-      SecretId: secretName,
-      // VersionStage defaults to AWSCURRENT if unspecified
-      VersionStage: 'AWSCURRENT',
-    });
-    const response = await client.send(command);
-    return response.SecretString;
-  } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
-  }
+  const command = new GetSecretValueCommand({
+    SecretId: secretName,
+    // VersionStage defaults to AWSCURRENT if unspecified
+    VersionStage: 'AWSCURRENT',
+  });
+  const response = await client.send(command);
+  return response.SecretString;
 };
