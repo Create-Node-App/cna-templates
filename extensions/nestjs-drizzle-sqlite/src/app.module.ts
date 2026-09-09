4bd908daa09f65d38b090b3db4ab172357c29229
ulises-jeremias <ulisescf.24@gmail.com>
2026-09-09 04:01:02 -0300

    fix(nestjs): .js import suffixes for drizzle extensions

--- extensions/nestjs-drizzle-sqlite/src/app.module.ts
+++ extensions/nestjs-drizzle-sqlite/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
-import { DrizzleProvider } from './db/drizzle.provider';
+import { DrizzleProvider } from './db/drizzle.provider.js';
 
 @Module({
   imports: [
